0f82e46b6d290700a17f8648533ce6fb29030ebb
Anthony Python <hensonstephen61@gmail.com>
2025-09-02 21:31:45 -0400

    Style Node Editor window like Content Browser

--- Editor/NodeEditorWindow.cpp
+++ Editor/NodeEditorWindow.cpp
@@ -8,21 +8,21 @@ using namespace wi::gui;
 
 void NodeEditorWindow::Create(EditorComponent* _editor)
 {
-	editor = _editor;
-	wi::gui::Window::Create("Node Editor");
-	SetSize(XMFLOAT2(800, 600));
-	SetMinimized(true);
-	SetVisible(false);
+        editor = _editor;
+        control_size = 30;
+        wi::gui::Window::Create("Node Editor");
+        RemoveWidget(&scrollbar_horizontal);
+        SetVisible(false);
 
-	addNodeButton.Create("Add Node");
-	addNodeButton.SetPos(XMFLOAT2(4, 4));
-	addNodeButton.SetSize(XMFLOAT2(100, 20));
-	addNodeButton.SetTooltip("Create a new node");
-	addNodeButton.OnClick([this](wi::gui::EventArgs args) {
-		XMFLOAT2 pos = XMFLOAT2(50 + nodes.size() * 120.0f, 80.0f);
-		AddNode("Node " + std::to_string(nodes.size()), pos);
-	});
-	AddWidget(&addNodeButton);
+        addNodeButton.Create("Add Node");
+        addNodeButton.SetTooltip("Create a new node");
+        addNodeButton.SetLocalizationEnabled(false);
+        addNodeButton.SetSize(XMFLOAT2(100, 20));
+        addNodeButton.OnClick([this](wi::gui::EventArgs args) {
+                XMFLOAT2 pos = XMFLOAT2(50 + nodes.size() * 120.0f, 80.0f);
+                AddNode("Node " + std::to_string(nodes.size()), pos);
+        });
+        AddWidget(&addNodeButton, wi::gui::Window::AttachmentOptions::NONE);
 }
 
 void NodeEditorWindow::AddNode(const std::string& name, const XMFLOAT2& position)
@@ -45,7 +45,40 @@ void NodeEditorWindow::AddNode(const std::string& name, const XMFLOAT2& position
 
 void NodeEditorWindow::Update(const wi::Canvas& canvas, float dt)
 {
-	wi::gui::Window::Update(canvas, dt);
+        wi::gui::Window::Update(canvas, dt);
+
+        SetShadowRadius(6);
+
+        static const float radius = 15;
+        for (int i = 0; i < arraysize(wi::gui::Widget::sprites); ++i)
+        {
+                sprites[i].params.enableCornerRounding();
+                sprites[i].params.corners_rounding[0].radius = radius;
+                sprites[i].params.corners_rounding[1].radius = radius;
+                sprites[i].params.corners_rounding[2].radius = radius;
+                sprites[i].params.corners_rounding[3].radius = radius;
+
+                addNodeButton.sprites[i].params.enableCornerRounding();
+                addNodeButton.sprites[i].params.corners_rounding[0].radius = radius;
+                addNodeButton.sprites[i].params.corners_rounding[1].radius = radius;
+                addNodeButton.sprites[i].params.corners_rounding[2].radius = radius;
+                addNodeButton.sprites[i].params.corners_rounding[3].radius = radius;
+        }
+
+        addNodeButton.SetShadowRadius(0);
+
+        for (auto& node : nodes)
+        {
+                node->window.SetShadowRadius(4);
+                for (auto& spr : node->window.sprites)
+                {
+                        spr.params.enableCornerRounding();
+                        spr.params.corners_rounding[0].radius = radius;
+                        spr.params.corners_rounding[1].radius = radius;
+                        spr.params.corners_rounding[2].radius = radius;
+                        spr.params.corners_rounding[3].radius = radius;
+                }
+        }
 }
 
 void NodeEditorWindow::Render(const wi::Canvas& canvas, wi::graphics::CommandList cmd) const
@@ -78,7 +111,11 @@ void NodeEditorWindow::Render(const wi::Canvas& canvas, wi::graphics::CommandLis
 
 void NodeEditorWindow::ResizeLayout()
 {
-	wi::gui::Window::ResizeLayout();
-	addNodeButton.SetPos(XMFLOAT2(4, 4));
+        wi::gui::Window::ResizeLayout();
+        const float padding = 4;
+
+        addNodeButton.Detach();
+        addNodeButton.SetPos(XMFLOAT2(translation.x + padding, translation.y + scale.y - addNodeButton.GetSize().y - padding));
+        addNodeButton.AttachTo(this);
 }
 
